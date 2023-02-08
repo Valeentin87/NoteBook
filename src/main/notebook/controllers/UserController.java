f3b9aa65c6f725aa613ccb903cb4b86b693ac44e
Valeentin87 <valeentin87@list.ru>
2023-02-09 00:42:19 +0300

    добавил цветов в консоль, итоговое сохранение

--- src/main/notebook/controllers/UserController.java
+++ src/main/notebook/controllers/UserController.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import model.*;
+import views.ViewUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserController {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
     private final Repository repository;
 
     public UserController(Repository repository) {
@@ -28,7 +32,7 @@ public class UserController {
     public void saveEach(){model.SaveModification.saveAllNote();}
     public void saveZip(){model.SaveModification.saveZipFile();}
     public void sendWeb(){
-        String web = views.ViewUser.prompt("\t\t\t\t\tВведите адрес электронной почты, на которую необходимо отправить данные книжки");
+        String web = views.ViewUser.prompt(ANSI_BLUE+"\t\t\t\t\tВведите адрес электронной почты, на которую необходимо отправить данные книжки"+ANSI_RESET);
         model.SaveModification.sendToWeb(web);
     }
 
